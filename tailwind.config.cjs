fc62c996ce9629e2fbf3ce9558aab97b14c525ed
052hide <hidetoshi1990@gmail.com>
2022-11-16 14:52:22 +0900

    create demo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  prefix: 'tw-',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
